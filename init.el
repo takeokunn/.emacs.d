1353c79e3f22b380bcc5fc59d4e1038d1f32c2dc
takeokunn <bararararatty@gmail.com>
2020-07-03 16:14:21 +0900

    Update

--- init.el
+++ init.el
@@ -1,1 +1,19 @@
 (load "~/.emacs.d/index")
+(put 'erase-buffer 'disabled nil)
+
+(load "~/.ghq/github.com/takeokunn/vlang-mode/vlang-mode.el")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(doom-themes-enable-bold t)
+ '(doom-themes-enable-italic t)
+ '(neo-theme (quote nerd2))
+ '(org-agenda-files nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(doom-modeline-bar ((t (:background "#6272a4")))))
