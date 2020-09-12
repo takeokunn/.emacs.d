736ad79be94251baea85c23e6b17cfbcbcaf57c9
takeokunn <bararararatty@gmail.com>
2020-09-12 19:15:36 +0900

    Update

--- Makefile
+++ Makefile
@@ -4,17 +4,6 @@ TOP_DIR := $(realpath $(dir $(lastword $(MAKEFILE_LIST))))
 .PHONY: all
 all: compile org-to-html
 
-.PHONY: install
-install: link cask-install cask-update
-
-.PHONY: cask-install
-cask-install:
-	cask install
-
-.PHONY: cask-update
-cask-update:
-	cask update
-
 .PHONY: clean
 clean:
 	rm -f  ~/.emacs.d/.junk/*
@@ -26,8 +15,9 @@ link:
 .PHONY: compile
 compile:
 	$(EMACS) -Q --batch --eval "(progn (require 'ob-tangle) (org-babel-tangle-file \"./index.org\" \"./index.el\" \"emacs-lisp\"))"
-	$(EMACS) -Q --batch -f batch-byte-compile "./index.el"
+	$(EMACS) -Q --batch -f batch-byte-compile index.el
 
 .PHONY: org-to-html
 org-to-html:
+	$(EMACS) index.org -Q --batch --eval "(progn (setq org-html-htmlize-output-type nil) (org-html-export-to-html))"
 	$(EMACS) index.org -Q --batch --eval "(progn (setq org-html-htmlize-output-type nil) (org-html-export-to-html))"
--- init.el
+++ init.el
@@ -4,11 +4,21 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(doom-modeline-buffer-file-name-style 'truncate-with-project)
+ '(doom-modeline-icon t)
+ '(doom-modeline-major-mode-icon nil)
+ '(doom-modeline-minor-modes nil)
+ '(doom-themes-enable-bold t)
+ '(doom-themes-enable-italic t)
+ '(package-archives
+   '(("gnu" . "https://elpa.gnu.org/packages/")
+     ("melpa" . "https://melpa.org/packages/")
+     ("org" . "https://orgmode.org/elpa/")))
  '(package-selected-packages
-   '(yasnippet-snippets web-mode vue-mode vimrc-mode use-package toml-mode tide terraform-mode swoop ssh-config-mode sql-indent smooth-scroll smex smartrep smartparens slim-mode scala-mode rustic ruby-refactor rubocop robe rainbow-delimiters racer processing-mode plantuml-mode php-mode open-junk-file omnisharp nyan-mode nginx-mode neotree nameless magit lsp-ui lsp-java lsp-haskell json-mode js2-refactor ivy-rich ivy-ghq hl-todo hindent helpful gradle-mode google-this go-mode gitignore-mode git-gutter-fringe general font-lock-studio fish-mode exec-path-from-shell esup emr emmet-mode elisp-slime-nav editorconfig doom-themes doom-modeline dockerfile-mode docker-compose-mode dhall-mode dashboard csv-mode counsel company-lsp company-glsl coffee-mode cmake-mode cider cask aweshell all-the-icons-ivy all-the-icons-dired ac-slime)))
+   '(git-gutter-fringe magit dap-mode lsp-ui yasnippet-snippets yasnippet slime-company company-lsp company-glsl company emr nyan-mode neotree hl-todo doom-themes doom-modeline all-the-icons-ivy all-the-icons cider nameless elisp-slime-nav rainbow-delimiters paredit web-mode vue-mode typescript-mode toml-mode terraform-mode ssh-config-mode slim-mode scala-mode rust-mode python-mode processing-mode plantuml-mode php-mode markdown-mode nginx-mode csv-mode lisp-mode json-mode js2-mode haskell-mode gradle-mode gitignore-mode fish-mode emmet-mode dockerfile-mode docker-compose-mode use-package leaf-convert leaf-keywords hydra ht general f el-get dash-functional blackout)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(doom-modeline-bar ((t (:background "#6272a4")))))
+ '(doom-modeline-bar ((t (:background "#6272a4"))) nil "Customized with leaf in doom-themes block"))
